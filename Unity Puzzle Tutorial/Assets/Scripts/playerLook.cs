233934baae0a2d2098453fb7552ae101f48a6555
Yiangos01 <ygeorgiou12@gmail.com>
2018-12-06 18:21:20 +0100

    test scene

---
+++ Unity Puzzle Tutorial/Assets/Scripts/playerLook.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerLook : MonoBehaviour {
+
+	[SerializeField] private string mouseXInputName, mouseYInputName;
+	[SerializeField] private float mouseSensitivity;
+
+	[SerializeField] private Transform playerBody;
+
+	private float xAxisClamp;
+
+	// Use this for initialization
+	void Awake () {
+		lockCursor ();
+		xAxisClamp = 0.0f;
+	}
+
+	void lockCursor (){
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		CameraRotation ();
+	}
+
+	private void CameraRotation(){
+		float mouseX = Input.GetAxis (mouseXInputName) * mouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis (mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+		xAxisClamp += mouseY;
+
+		if (xAxisClamp > 90.0f) {
+			xAxisClamp = 90.0f;
+			mouseY = 0.0f;
+			ClampXAxisRotationToValue (270.0f);
+		}
+		if (xAxisClamp < -90.0f) {
+			xAxisClamp = -90.0f;
+			mouseY = 0.0f;
+			ClampXAxisRotationToValue (90.0f);
+		}
+		transform.Rotate (Vector3.left * mouseY);
+		playerBody.Rotate (Vector3.up * mouseX);
+	}
+
+	private void ClampXAxisRotationToValue(float value)
+	{
+		Vector3 eulerRotation = transform.eulerAngles;
+		eulerRotation.x = value;
+		transform.eulerAngles = eulerRotation;
+	}
+}
